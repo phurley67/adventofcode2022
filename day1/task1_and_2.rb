b37329fe1451ff59d35fd6a9b4fbe7ded9d616c3
Patrick Hurley <phurley@invoca.com>
2022-12-01 12:31:39 -0500

    Include example reusing the enumerator

---
+++ day1/task1_and_2.rb
@@ -0,0 +1,13 @@
+calories_by_elf = ARGF.           # Input file stream
+  each_line.                      # get each line (will include newlines)
+  map(&:to_i).                    # convert every line to an integer, blank lines will be zero
+  slice_when { |e| e.zero? }.     # break the array up by elf -> [[12,2,0],[20,0],...]
+  map(&:sum).                     # collapse each elf sub-array into a single value
+  sort.
+  reverse
+
+# using sort.reverse first/take here as max would repeat that relatively expensive work
+# if you knew you never needed more than 3, max(3).sort.reverse might be even better
+
+puts "Max calories 1 elf #{calories_by_elf.first}"
+puts "Max calories 3 elves #{calories_by_elf.take(3).sum}"
