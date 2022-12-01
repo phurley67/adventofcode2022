5513f34f0da2369b8c3495e7deb727dee90d3f21
Patrick Hurley <phurley@invoca.com>
2022-12-01 12:20:23 -0500

    Use max enumerable method, per Devin Roark -- much nicer

--- day1/task1.rb
+++ day1/task1.rb
@@ -3,6 +3,4 @@ puts ARGF.                        # Input file stream
   map(&:to_i).                    # convert every line to an integer, blank lines will be zero
   slice_when { |e| e.zero? }.     # break the array up by elf -> [[12,2,0],[20,0],...]
   map(&:sum).                     # collapse each elf sub-array into a single value
-  sort.                           # don't you hate comments that 
-  reverse.                        # say the same thing as the code
-  first                           # after reversing the first element will be the largest and our answer
+  max
--- day1/task2.rb
+++ day1/task2.rb
@@ -3,7 +3,5 @@ puts ARGF.                        # Input file stream
   map(&:to_i).                    # convert every line to an integer, blank lines will be zero
   slice_when { |e| e.zero? }.     # break the array up by elf -> [[12,2,0],[20,0],...]
   map(&:sum).                     # collapse each elf sub-array into a single value
-  sort.                           # don't you hate comments that 
-  reverse.                        # say the same thing as the code
-  take(3).                        # grab the three largest (after reverse sorting)
-  sum                             # we have our answer
+  max(3).                         # don't you hate comments that
+  sum                             # just say what the code says?
