7f3b98fb17d6cea0707fea90654f4bba8766f6e0
Patrick Hurley <phurley@invoca.com>
2022-12-02 10:29:35 -0500

    Updated with alorithmic options

--- day2/task1.rb
+++ day2/task1.rb
@@ -1,13 +1,51 @@
+WIN = 6
+TIE = 3
+LOSS = 0
+
+ROCK = 1
+PAPER = 2
+SCISSOR = 3
+
+# Originally I just wrote this table
 SCORE_KEY = {
-  "A X" => 3 + 1,
-  "A Y" => 6 + 2,
-  "A Z" => 0 + 3,
-  "B X" => 0 + 1,
-  "B Y" => 3 + 2,
-  "B Z" => 6 + 3,
-  "C X" => 6 + 1,
-  "C Y" => 0 + 2,
-  "C Z" => 3 + 3
+  "A X" => TIE  + ROCK,
+  "A Y" => WIN  + PAPER,
+  "A Z" => LOSS + SCISSOR,
+  "B X" => LOSS + ROCK,
+  "B Y" => TIE  + PAPER,
+  "B Z" => WIN  + SCISSOR,
+  "C X" => WIN  + ROCK,
+  "C Y" => LOSS + PAPER,
+  "C Z" => TIE  + SCISSOR
 }
 
+def score(game)
+  if game.match(/\A[A-C] [X-Z]/) 
+    # grab the low three bits of each ascii value
+    p1 = game[0].ord[0..2]
+    p2 = game[2].ord[0..2]
+
+    # Yikes!
+    # subtracting the p2 from p1 mod 3 gives a 0,1,2 for W,T,L respectively -- found through fiddling
+    # 2 - that value changes W,T,L to 2,1,0
+    # * 3 - that value changes W,T,L to 6,3,0
+    # p2 will be 0,1,2 for R,P,S
+    # p2 + 1 will be 1,2,3 for R,P,S
+    # add it together and you get the correct score
+    (2 - ((p1 - p2) % 3)) * 3 + p2 + 1
+  end
+end
+
+# I sort of like the table approach, but if you wanted 
+# something more algorithmic -- for example if the 
+# table was too big to maintain, you could still use
+# a hash to memoize the work like so
+# 
+# SCORE_KEY = Hash.new { |h,k| h[k] = score(k) }
+#
+# or just 
+#
+# puts ARGF.each_line.sum { |k| score(k) }
+
 puts ARGF.each_line.sum { |k| SCORE_KEY[k.chomp] }
+
--- day2/task2.rb
+++ day2/task2.rb
@@ -1,14 +1,38 @@
+WIN = 6
+TIE = 3
+LOSS = 0
+
+ROCK = 1
+PAPER = 2
+SCISSOR = 3
+
 SCORE_KEY = {
-  "A X" => 0 + 3,
-  "A Y" => 3 + 1,
-  "A Z" => 6 + 2,
-  "B X" => 0 + 1,
-  "B Y" => 3 + 2,
-  "B Z" => 6 + 3,
-  "C X" => 0 + 2,
-  "C Y" => 3 + 3,
-  "C Z" => 6 + 1
+  "A X" => LOSS + SCISSOR,
+  "A Y" =>  TIE +    ROCK,
+  "A Z" =>  WIN +   PAPER,
+  "B X" => LOSS +    ROCK,
+  "B Y" =>  TIE +   PAPER,
+  "B Z" =>  WIN + SCISSOR,
+  "C X" => LOSS +   PAPER,
+  "C Y" =>  TIE + SCISSOR,
+  "C Z" =>  WIN +    ROCK
 }
 
+def score(game)
+  if game.match(/\A[A-C] [X-Z]/) 
+    # grab the low three bits of each ascii value
+    p1 = game[0].ord[0..2]
+    p2 = game[2].ord[0..2]
+
+    # Yikes!
+    # p2 will be 0,1,2 for L,T,W
+    # p2 * 3 will be 0,3,6
+    # more fiddling, finds (p1 + p2 + 1) % 3 + 1, gives 1,2,3 for R,P,S played to win based on X,Y,Z being L,T,W
+    # -- probably should have used game[2].ord - 'W'.ord...
+    p2 * 3 + ((p1 + p2 + 1) % 3) + 1
+  end
+end
+
+#puts ARGF.each_line.sum { |k| score(k) }
 puts ARGF.each_line.sum { |k| SCORE_KEY[k.chomp] }
 
